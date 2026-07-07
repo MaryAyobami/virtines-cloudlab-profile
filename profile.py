623c7bb859e2845ddc1dc5cbc2ff838e545449b3
Mary Ayobami Ogunmola <ogunmolamaryayobami@gmail.com>
2026-07-07 17:07:07 -0600

    Add profile.py for RSpec request setup

---
+++ profile.py
@@ -0,0 +1,19 @@
+# Import the Portal object.
+import geni.portal as portal
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context, needed to defined parameters
+pc = portal.Context()
+
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+ 
+# Add a raw PC to the request.
+node = request.RawPC("node")
+# Set the OS image for the node.
+node.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD'
+node.hardware_type = 'c6525-25g'
+
+# Print the RSpec to the enclosing page.
+pc.printRequestRSpec(request)
